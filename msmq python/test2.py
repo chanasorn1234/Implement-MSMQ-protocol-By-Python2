94f852d07235a17c94da9d0d7ff1c9bc15aaef6e
Chanasorn Siriwong <69181563+chanasorn1234@users.noreply.github.com>
2022-06-09 20:31:29 +0700

    Add files via upload

--- msmq python/test2.py
+++ msmq python/test2.py
@@ -7,6 +7,18 @@ import struct
 from time import sleep
 import pythoncom
 pattern = re.compile('x')
+
+# qinfo_id=win32com.client.Dispatch("MSMQ.MSMQQueueInfo")
+# computer_name = os.getenv('COMPUTERNAME')
+# qinfo_id.FormatName="direct=os:"+computer_name+"\\PRIVATE$\\myqueue"
+# pre_id = qinfo_id.Open(1,0)
+# timeout_sec = 1.0
+# keepid = pre_id.Peek(pythoncom.Empty, pythoncom.Empty, timeout_sec * 1000)
+# print(keepid.Id[16:20],'')
+# pre_id.Close()
+
+
+
 qinfo=win32com.client.Dispatch("MSMQ.MSMQQueueInfo")
 computer_name = os.getenv('COMPUTERNAME')
 qinfo.FormatName="direct=os:"+computer_name+"\\PRIVATE$\\myqueue"
@@ -17,35 +29,35 @@ print("Body :",msg.Body)
 print("ID:",str(msg.SourceMachineGuid))
 print("ID2:",msg.LookupId)
 print("Time:",msg.SentTime)
-# print(int(bytes(msg.Id).e),"")
+
 result = msg.Id[16:20]
 # result = b'\x0f\xc8\x01\x00'
 print(type(result))
 print(result,'')
-print(struct.unpack('4B',result))
-num = struct.unpack('4B',result)
+print(struct.unpack('<HH',result))
+num = struct.unpack('<HH',result)
 message_num = 0
 for i in num:
     message_num += i
 print(message_num)
 queue.Close()
 
-sleep(1)
+# sleep(1)
 
-resqinfo = win32com.client.Dispatch("MSMQ.MSMQQueueInfo")
-res_computer_name = os.getenv('COMPUTERNAME')
-resqinfo.FormatName="direct=os:"+res_computer_name+"\\PRIVATE$\\myqueue"
-resqeue = resqinfo.Open(2,0)
-resmsg = win32com.client.Dispatch("MSMQ.MSMQMessage")
-resmsg.Body = "KuyKob"
-resmsg.Label = "ResTestMsg"
-print(type(msg.Id))
-resmsg.CorrelationId = msg.Id
-res_destination = win32com.client.Dispatch("MSMQ.MSMQDestination")
-res_destination.FormatName = "direct=os:"+computer_name+"\\PRIVATE$\\myqueue"
-resmsg.ResponseDestination = res_destination
-resmsg.Send(resqeue)
-resqeue.Close()
+# resqinfo = win32com.client.Dispatch("MSMQ.MSMQQueueInfo")
+# res_computer_name = os.getenv('COMPUTERNAME')
+# resqinfo.FormatName="direct=os:"+res_computer_name+"\\PRIVATE$\\myqueue"
+# resqeue = resqinfo.Open(2,0)
+# resmsg = win32com.client.Dispatch("MSMQ.MSMQMessage")
+# resmsg.Body = "KuyKob"
+# resmsg.Label = "ResTestMsg"
+# print(type(resmsg.Id))
+# resmsg.CorrelationId = msg.Id
+# res_destination = win32com.client.Dispatch("MSMQ.MSMQDestination")
+# res_destination.FormatName = "direct=os:"+computer_name+"\\PRIVATE$\\myqueue"
+# resmsg.ResponseDestination = res_destination
+# resmsg.Send(resqeue)
+# resqeue.Close()
 
 
 
