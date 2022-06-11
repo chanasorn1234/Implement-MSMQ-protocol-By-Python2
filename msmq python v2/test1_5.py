e433e867c895736656e05f2734a0e7d8035bb55c
Chanasorn Siriwong <69181563+chanasorn1234@users.noreply.github.com>
2022-06-12 00:43:04 +0700

    Add files via upload

---
+++ msmq python v2/test1_5.py
@@ -0,0 +1,24 @@
+from time import sleep
+import win32com.client
+import os
+import struct
+from time import sleep
+import pythoncom
+
+
+destOrig = win32com.client.Dispatch("MSMQ.MSMQDestination")
+destResp = win32com.client.Dispatch("MSMQ.MSMQDestination")
+msg = win32com.client.Dispatch("MSMQ.MSMQMessage")
+
+res_computer_name = os.getenv('COMPUTERNAME')
+
+destOrig.Formatname = "direct=os:"+res_computer_name+"\\PRIVATE$\\myqueue"
+destResp.Formatname = "direct=os:"+res_computer_name+"\\PRIVATE$\\myqueue"
+
+msg.ResponseDestination = destResp
+
+msg.Label = "Test Message: Response"
+
+msg.Send(destOrig)
+msg.Send(destOrig)
+print("sendrequest done")
